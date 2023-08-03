4ed13f613c7a5b82f4e2042c6a7df70376ea8764
Bram Hendriks <bhendriks@kembit.nl>
2023-08-03 12:16:42 +0200

    Report User  Added
    Fixed Migration, Fixed BlackList And delete User. added basics for following user

---
+++ Smort-api.Object/User/UserData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smort_api.Object.User
+{
+    public class UserData
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
