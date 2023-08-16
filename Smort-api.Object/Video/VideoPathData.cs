b9463bbbb50882b6cc753edf0e4af27e89eccd60
Bram Hendriks <bhendriks@kembit.nl>
2023-08-16 19:27:43 +0200

    DockerFile and workflow Update

---
+++ Smort-api.Object/Video/VideoPathData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smort_api.Object.Videos
+{
+    public class VideoPathData
+    {
+        public string? File_Location { get; set; }
+    }
+}
